368173b10cd81e985d8b3ba331e99e7cff3566a0
Andrey Zhuk <zhuk.andrey.a@gmail.com>
2015-03-01 12:54:37 +0200

    second commit

--- app/code/Atwix/HelloWorld/Controller/Index/Index.php
+++ app/code/Atwix/HelloWorld/Controller/Index/Index.php
@@ -4,11 +4,6 @@ namespace Atwix\HelloWorld\Controller\Index;
 class Index extends \Magento\Framework\App\Action\Action
 {
 
-    /**
-     * Default customer account page
-     *
-     * @return void
-     */
     public function execute()
     {
         echo 'Hello World! Magento 2';
